7c9857c4cac61a5ca07473e0fa1d792ac69beb2a
Kyrylo Shestakov <pochtash100@gmail.com>
2026-03-20 21:46:36 +0100

    Refactoring

--- app/src/main/java/com/termite/wallet/AppConfigs.java
+++ app/src/main/java/com/termite/wallet/AppConfigs.java
@@ -2,14 +2,14 @@ package com.termite.wallet;
 
 public class AppConfigs {
     private static final AppConfigs instance = new AppConfigs();
-    private String serverIp = "192.168.220.55";
-    private int serverPort = 5009;
     private AppConfigs() {
     }
     public static AppConfigs getInstance() {
         return instance;
     }
     public String getBaseUrl() {
+        String serverIp = "192.168.220.55";
+        int serverPort = 5009;
         return "http://" + serverIp + ":" + serverPort + "/api/main/";
     }
 }
